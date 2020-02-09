7d2afe720163575ff883a45d1928ffde45ed4ac2
gogolon <gogolonsyachiku@gmail.com>
2020-02-09 21:35:13 +0900

    env追加

--- 01-laravel-up.sh
+++ 01-laravel-up.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+# コンテナ名
+CONTAINER = laravel
 # Laravelプロジェクト名/DB名
 PROJECT=board
 # DB Password
 PASSWORD=P@ssw0rd
 
 # Laravelプロジェクトを作詞
-docker exec -it laravel composer create-project "laravel/laravel=6.0.*" $PROJECT
-docker exec -it laravel /bin/bash -c "cd /var/www/html/$PROJECT; chmod -R 777 ./storage; chmod -R 777 ./bootstrap/cache/"
+docker exec -it $CONTAINER composer create-project "laravel/laravel=6.0.*" $PROJECT
+docker exec -it $CONTAINER /bin/bash -c "cd /voar/www/html/$PROJECT; chmod -R 777 ./storage; chmod -R 777 ./bootstrap/cache/"
+docker exec -it $CONTAINER composer require laravel/ui
+docker exec -it $CONTAINER php artisan ui vue --auth
+
 
 # httpd.confをバックアップ後、Document Rootなどを書き換え
 cp ./apache-php/httpd.conf ./apache-php/httpd.conf.bk
@@ -16,15 +21,15 @@ sed -i -e "s/\<Directory \"\/var\/www\/html\">/\<Directory \"\/var\/www\/html\/$
 sed -i -e "s/AllowOverride None/AllowOverride All/" ./apache-php/httpd.conf
 
 # .envのDB設定を書き換え
-docker exec -it laravel /bin/bash -c "cd /var/www/html/$PROJECT; sed -i -e \"s/DB_HOST=\(.*\)/DB_HOST=mysql/g\" .env"
-docker exec -it laravel /bin/bash -c "cd /var/www/html/$PROJECT; sed -i -e \"s/DB_DATABASE=\(.*\)/DB_DATABASE=$PROJECT/g\" .env"
-docker exec -it laravel /bin/bash -c "cd /var/www/html/$PROJECT; sed -i -e \"s/DB_PASSWORD=\(.*\)/DB_PASSWORD=$PASSWORD/g\" .env"
+docker exec -it $CONTAINER /bin/bash -c "cd /var/www/html/$PROJECT; sed -i -e \"s/DB_HOST=\(.*\)/DB_HOST=mysql/g\" .env"
+docker exec -it $CONTAINER /bin/bash -c "cd /var/www/html/$PROJECT; sed -i -e \"s/DB_DATABASE=\(.*\)/DB_DATABASE=$PROJECT/g\" .env"
+docker exec -it $CONTAINER /bin/bash -c "cd /var/www/html/$PROJECT; sed -i -e \"s/DB_PASSWORD=\(.*\)/DB_PASSWORD=$PASSWORD/g\" .env"
 
 # Laravel Migrate実行
-docker exec -it laravel /bin/bash -c "cd /var/www/html/$PROJECT; php artisan migrate"
+docker exec -it $CONTAINER /bin/bash -c "cd /var/www/html/$PROJECT; php artisan migrate"
 
 # Laravelコンテナの再起動
-docker restart laravel
+docker restart $CONTAINER
 
 # 以下を確認します。
 # http://localhost/
