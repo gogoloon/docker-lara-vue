23e6b108966cf1aacc552dec3441678862b94bac
gogolon <gogolonsyachiku@gmail.com>
2020-02-11 20:56:53 +0900

    vue.jsのインストール追加

--- init_setup.sh
+++ init_setup.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
+# .envファイルの読み込み
 . .env
 
-# コンテナ名+Laravelプロジェクト名+DB名
-#CONTAINER=lara-vue-img
-# DB Password
-#PASSWORD=P@ssw0rd
-
-# Laravelプロジェクトを作詞
+# Laravelプロジェクトを作成
 docker exec -it $COMPOSE_PROJECT_NAME composer create-project "laravel/laravel=6.0.*" $COMPOSE_PROJECT_NAME
 docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; chmod -R 777 ./storage; chmod -R 777 ./bootstrap/cache/"
 docker exec -it $COMPOSE_PROJECT_NAME composer require laravel/ui
 docker exec -it $COMPOSE_PROJECT_NAME php artisan ui vue --auth
-
+docker exec -it $COMPOSE_PROJECT_NAME npm install
+docker exec -it $COMPOSE_PROJECT_NAME npm run dev
 
 # はじめにhttp.confを初期化する
 cp ./apache-php/httpd.conf.ori ./apache-php/httpd.conf
@@ -21,9 +18,9 @@ sed -i '' "s/\<Directory \"\/var\/www\/html\">/\<Directory \"\/var\/www\/html\/$
 sed -i '' "s/AllowOverride None/AllowOverride All/" ./apache-php/httpd.conf
 
 # .envのDB設定を書き換え
-docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; sed -i \"s/DB_HOST=\(.*\)/DB_HOST=mysql/g\" .env"
-docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; sed -i \"s/DB_DATABASE=\(.*\)/DB_DATABASE=$COMPOSE_PROJECT_NAME/g\" .env"
-docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; sed -i \"s/DB_PASSWORD=\(.*\)/DB_PASSWORD=$PASSWORD/g\" .env"
+docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; sed -i '' \"s/DB_HOST=\(.*\)/DB_HOST=mysql/g\" .env"
+docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; sed -i '' \"s/DB_DATABASE=\(.*\)/DB_DATABASE=$COMPOSE_PROJECT_NAME/g\" .env"
+docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; sed -i '' \"s/DB_PASSWORD=\(.*\)/DB_PASSWORD=$PASSWORD/g\" .env"
 
 # Laravel Migrate実行
 docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PROJECT_NAME; php artisan migrate"
@@ -31,7 +28,7 @@ docker exec -it $COMPOSE_PROJECT_NAME /bin/bash -c "cd /var/www/html/$COMPOSE_PR
 # Laravelコンテナの再起動
 docker restart $COMPOSE_PROJECT_NAME
 
-echo "---------------------------------"
+echo "-------------------------------------"
 echo "Laravelの初期設定が完了しました!"
 echo "Laravelプロジェクト：$COMPOSE_PROJECT_NAME"
 echo "以下のURLへアクセスしてください!"
@@ -43,5 +40,5 @@ echo "DB情報は以下となります"
 echo " - DB port : $PORT_DB"
 echo " - DB name : $COMPOSE_PROJECT_NAME"
 echo " - DB root password : $PASSWORD"
-echo "---------------------------------"
+echo "-------------------------------------"
 
